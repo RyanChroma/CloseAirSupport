575daec5fa6b05cc98a91266d30f5795f6cf5bda
RyanCua <ttff2ryan@gmail.com>
2022-09-21 03:44:24 +0700

    E

--- Close Air Support/Assets/Scripts/PlanePilot.cs
+++ Close Air Support/Assets/Scripts/PlanePilot.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class PlanePilot : MonoBehaviour
 {
+	public float speed = 90.0f;
+
 	private void Update()
 	{
+		//PLANE SPEED
+		transform.position += transform.forward * Time.deltaTime * 90.0f;
+		speed -= transform.forward.y * Time.deltaTime * 2.0f;
 		transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
+
+		float terrainHeightWhereWeAre = Terrain.activeTerrain.SampleHeight(transform.position);
+
+		if(terrainHeightWhereWeAre > transform.position.y)
+		{
+			transform.position = new Vector3(transform.position.x, terrainHeightWhereWeAre, transform.position.z);
+		}
 	}
 }

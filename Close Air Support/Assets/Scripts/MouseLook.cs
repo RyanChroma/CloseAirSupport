5ba31cb326a5d9f9334969280831feb91e72760a
RyanCua <ttff2ryan@gmail.com>
2022-09-22 19:30:48 +0700

    Change back to war thunder camera

--- Close Air Support/Assets/Scripts/MouseLook.cs
+++ Close Air Support/Assets/Scripts/MouseLook.cs
@@ -16,6 +16,12 @@ public class MouseLook : MonoBehaviour
 
 	void Update()
     {
+        //CAMERA WILL NOT ROTATE CLOCKWISE/COUNTER CLOCKWISE
+        Vector3 mainCameraController = transform.position - transform.forward * 10.0f + Vector3.up * 5.0f; //transform is relative, vector is universal.
+		float bias = 0.96f;
+		Camera.main.transform.position = Camera.main.transform.position * bias + mainCameraController * (1.0f - bias);
+		Camera.main.transform.LookAt(transform.position + transform.forward * 30.0f);
+
         if (Input.GetKey(KeyCode.C))
         {
             float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
